199268802e670fe4238e72734219be5de0dc80bf
Andrei L <andrewlopukhov@gmail.com>
2020-09-01 23:42:33 +0300

    bugfix. Removed unnecessary annotation.

--- gusp/src/main/java/io/github/alopukhov/gusp/annotations/GuspConfiguration.java
+++ gusp/src/main/java/io/github/alopukhov/gusp/annotations/GuspConfiguration.java
@@ -3,7 +3,6 @@ package io.github.alopukhov.gusp.annotations;
 import io.github.alopukhov.gusp.lifecycle.WithSmartLifecyclePostprocessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Crates support beans for all features of GUSP project.
